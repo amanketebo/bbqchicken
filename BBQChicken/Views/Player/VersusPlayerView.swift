d32d83b91723dd232aa56b6cecbb3eab6d954ec0
Aman Ketebo <amanketebo@yahoo.com>
2020-12-10 11:36:57 -0500

    Removes tokens

--- BBQChicken/Views/Player/VersusPlayerView.swift
+++ BBQChicken/Views/Player/VersusPlayerView.swift
@@ -32,10 +32,10 @@ struct VersusPlayerView: View {
                 }
                 .frame(minWidth: 0,
                        idealWidth: 100,
-                       maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
-                       minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/,
-                       idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,
-                       maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
+                       maxWidth: .infinity,
+                       minHeight: 0,
+                       idealHeight: 100,
+                       maxHeight: .infinity,
                        alignment: .topTrailing)
                 .offset(x: 20, y: -20)
             }
